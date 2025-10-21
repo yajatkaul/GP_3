bf51259629946e71bf53fd0d07f98d70110f510e
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2025-10-21 20:39:55 +0530

    Got into uniforms

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static void GLCheckError() {
+    while (GLenum error = glGetError()) {
+        cout << "[OpenGL Error] (" << error << ")" << endl;
+    }
+}
+
 static string ParseShader(const string &filepath)
 {
     ifstream file(filepath);
@@ -93,15 +103,22 @@ int main(void)
 
     cout << glGetString(GL_VERSION) << endl;
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-        0.0f, 0.5f,
-        0.5f, -0.5f};
+    float positions[] = {
+        -0.5f,-0.5f, // 0
+         0.5f,-0.5f, // 1
+         0.5f, 0.5f, // 2
+        -0.5f, 0.5f, // 3
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
 
     unsigned int buffer;
     // Create buffer returns the id and assings it to buffer
     glGenBuffers(1, &buffer);
-    // Purpose of buffer array here
+    // Bind current array buffer array here
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     // Add data in there
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
@@ -109,18 +126,32 @@ int main(void)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
 
+    unsigned int ibo;
+    // this time creates an index buffer
+    glGenBuffers(1, &ibo);
+    // binds a current array buffer here
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    // puts index data in there
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     string vertexShader = ParseShader("./shaders/vertex.shader");
     string fragmentShader = ParseShader("./shaders/fragment.shader");
 
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
 
+    int location = glGetUniformLocation(shader, "u_Color");
+    glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        GLClearError();
+        // From the index buffer gets the position data and draws them
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCheckError();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
